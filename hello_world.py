d10e09422dad54bf0b12bedf6e67231fe35942dc
Rassol1401 <rassolenkomaksim1@gmail.com>
2025-01-09 10:40:15 +0300

    Add files via upload

--- hello_world.py
+++ hello_world.py
@@ -7,8 +7,8 @@ sander_name="Максим"
 sender_email="devmanorg@yandex.ru"
 recipient_email="rassolenko.maxim@yandex.ru"
 letter="""/
-From: devmanorg@yandex.ru
-To: rassolenko.maxim@yandex.ru
+From: %sender_email%
+To: %recipient_email%
 Subject: Приглашение!
 Content-Type: text/plain; charset="UTF-8
 
@@ -26,13 +26,13 @@ Content-Type: text/plain; charset="UTF-8
 Все проекты — они же решение наших задачек — можно разместить на твоём GitHub. Работодатели такое оценят. 
 
 Регистрируйся → %website%  
-На курсы, которые еще не вышли, можно подписаться и получить уведомление о релизе сразу на имейл.""".replace("%friend_name%",friend_name).replace("%my_name%",sander_name).replace("%website%",site_name).replace("devmanorg@yandex.ru",sender_email).replace("rassolenko.maxim@yandex.ru",recipient_email)
+На курсы, которые еще не вышли, можно подписаться и получить уведомление о релизе сразу на имейл.""".replace("%friend_name%",friend_name).replace("%my_name%",sander_name).replace("%website%",site_name).replace("%sender_email%",sender_email).replace("%recipient_email%",recipient_email)
 letter = letter.encode("UTF-8")
 load_dotenv()
 login= os.getenv('LOGIN')
 password= os.getenv('TOKEN')
 server = smtplib.SMTP_SSL("smtp.yandex.ru:465")
 server.login(login,password)
-server.sendmail(sender_email,recipient_email, letter)
+server.sendmail(sender_email,recipient_email,letter)
 server.quit()
 
